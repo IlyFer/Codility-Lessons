6b029ccc798fda7ca4bdca20ffa7cf36463950fb
Ilyes <ilyes.ferchichi@yahoo.fr>
2023-03-06 20:01:03 +0100

    Added the solution for the Brackets exercise.

---
+++ 7_Stacks_and_Queues/Brackets.cpp
@@ -0,0 +1,43 @@
+#include <stack>
+
+char matchingCharacter(const char& element) {
+    char result = '\0';
+
+    if (element == '(') {
+        result = ')';
+    } else if (element == '[') {
+        result = ']';
+    } else {
+        result = '}';
+    }
+
+    return result;
+}
+
+int solution(string &S) {
+    // write your code in C++14 (g++ 6.2.0)
+    stack<char> stack;
+    int result = 1;
+    
+    for (char x : S) {
+        if (x == '(' || x == '[' || x == '{') {
+            stack.push(x);
+        } else {
+            if (stack.empty()) {
+                result = 0;
+                break;
+            } else if (x == matchingCharacter(stack.top())) {
+                stack.pop();
+            } else {
+                result = 0;
+                break;
+            }
+        }
+    }
+
+    if (!stack.empty()) {
+        result = 0;
+    }
+
+    return result;
+}
