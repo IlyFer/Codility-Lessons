b5da3fe1fc5c26bcc206541ffc0c3a19d8e284e3
Ilyes <ilyes.ferchichi@yahoo.fr>
2023-03-06 19:48:27 +0100

    Added the solution for the PassingCars exercise.

---
+++ 5_Prefix_Sums/PassingCars.cpp
@@ -0,0 +1,19 @@
+const int maxPassingCars = 1000000000;
+int solution(vector<int> &A) {
+    int numPassingCars = 0;
+    int numEast = 0;
+
+    for (int car : A) {
+        if (car == 0) {
+            numEast++;
+        } else if (numEast > 0) {
+            numPassingCars += numEast;
+            if (numPassingCars > maxPassingCars) {
+                numPassingCars = -1;
+                break;
+            }
+        }
+    }
+
+    return numPassingCars;
+}
