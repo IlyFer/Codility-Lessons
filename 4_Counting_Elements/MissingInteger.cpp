cd18904810ec47ebd36e0bca235ee356f6c6083a
Ilyes <ilyes.ferchichi@yahoo.fr>
2023-03-06 19:41:59 +0100

    Added the solution for the MissingInteger exercise.

---
+++ 4_Counting_Elements/MissingInteger.cpp
@@ -0,0 +1,20 @@
+const unsigned maxValue = 1000000;
+int solution(vector<int> &A) {
+    vector<unsigned> foundValues(maxValue + 1, 0);
+    unsigned smallestMissing = 0;
+
+    for (int x : A) {
+        if (x > 0) {
+            foundValues[x] = 1;
+        }
+    }
+
+    for (unsigned i = 1; i <= maxValue; i++) {
+        if (foundValues[i] == 0) {
+            smallestMissing = i;
+            break;
+        }
+    }
+
+    return smallestMissing;
+}
