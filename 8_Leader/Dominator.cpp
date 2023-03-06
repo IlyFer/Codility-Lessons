dd5ee760cab1b5c28590d06b855a3b2f82a1c4cd
Ilyes <ilyes.ferchichi@yahoo.fr>
2023-03-06 20:07:28 +0100

    Added the solution for the Dominator exercise.

---
+++ 8_Leader/Dominator.cpp
@@ -0,0 +1,34 @@
+int solution(vector<int> &A) {
+    int result = -1;
+    int size = 0;
+    int candidate;
+
+    for (int x : A) {
+        if (size == 0) {
+            size++;
+            candidate = x;
+        } else if (x != candidate) {
+            size--;
+        } else {
+            size++;
+        }
+    }
+
+    if (size > 0) {
+        int index = -1;
+        unsigned count = 0;
+        for (unsigned i = 0; i < A.size(); i++) {
+            if (A[i] == candidate) {
+                index = i;
+                count++;
+            }
+        }
+
+        if (count > A.size() / 2) {
+            result = index;
+        }
+        
+    }
+
+    return result;
+}
